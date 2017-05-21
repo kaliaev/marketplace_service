acfecfcf74f928b4a8a0bb363db7c9e04edda728
Justin <pro.surfer@outlook.com>
2017-05-21 15:55:45 -0400

    Adds devise user model

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ end
 
 #Api gems
 gem 'active_model_serializers'
+gem 'devise'
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Api definition
   namespace :api, defaults: { format: :json },
